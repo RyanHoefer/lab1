8318b205069b032003849a385916c01a12eb534a
Hoefer <rhoefer@iastate.edu>
2026-04-08 18:45:58 -0500

    lab 10 start

---
+++ servo.c
@@ -0,0 +1,67 @@
+/*
+ * ping.c
+ *
+ *  Created on: Apr 1, 2026
+ *      Author: rhoefer
+ */
+
+
+/**
+ * Driver for ping sensor
+ * @file ping.c
+ * @author
+ */
+
+#include "ping.h"
+#include "Timer.h"
+#include "lcd.h"
+
+
+// Global shared variables
+// Use extern declarations in the header file
+
+
+
+
+void servo_init (void){
+    SYSCTL_RCGCGPIO_R |= 0x02;// clock to port gpiob
+    SYSCTL_RCGCTIMER_R |= 0x20;// clock to pin 5
+
+    while ((SYSCTL_PRGPIO_R & 0x2) == 0) {};
+    while ((SYSCTL_PRTIMER_R & 0x20) == 0) {};
+
+    GPIO_PORTB_DEN_R |= 0x20; //  enable PB5 as digital pin
+    GPIO_PORTB_AFSEL_R |= 0x20; // make PB5 alternate function
+    //GPIO_PORTB_PCTL_R &= ~0xF00000;// reset mux for the timer
+    GPIO_PORTB_PCTL_R |= 0x700000;// set 7 to the mux value for the timer
+
+
+    TIMER1_CTL_R  &= ~0x100;// disable the timer
+    TIMER1_CFG_R  |= 0x4; // set 16 bit mode
+    TIMER1_TBMR_R |= 0xA;// tbams and 0x2 for low group
+    TIMER1_TBMR_R &= ~0x4;// reset bit 2
+    TIMER1_TBILR_R |= 0xE200;// set max value
+    TIMER1_TBPR_R |= 0x04;//
+    TIMER1_TBMATCHR_R |= 0xDF94;
+    TIMER1_TBPMR_R |= 0x04;
+
+   // Configure and enable the timer
+
+    TIMER1_CTL_R |= 0x100; //
+}
+
+void servo_move(uint16_t degrees){
+
+
+    TIMER1_TBMATCHR_R = degrees;
+
+
+
+}
+
+
+
+
+
+
+
---
+++ servo.h
@@ -0,0 +1,30 @@
+/*
+ * servo.h
+ *
+ *  Created on: Apr 8, 2026
+ *      Author: rhoefer
+ */
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <inc/tm4c123gh6pm.h>
+#include "driverlib/interrupt.h"
+
+
+
+/**
+ * Initialize ping sensor. Uses PB3 and Timer 3B
+ */
+void servo_init(void);
+void servo_move(uint16_t degrees);
+
+
+
+
+
+#endif /* SERVO_H_ */
